58ee8211ba972b3ee63118fb57ce84c448f356e5
gyuchanlee <hoo788@gmail.com>
2022-10-22 23:09:55 +0900

    Initial commit

---
+++ src/test/java/com/dodo/bootawsproject/BootAwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.bootawsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAwsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
