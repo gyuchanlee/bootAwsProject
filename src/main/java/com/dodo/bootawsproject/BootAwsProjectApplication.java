634edb7d7da96dd82a4573042fe0b58ee84d8f05
gyuchanlee <hoo788@gmail.com>
2022-10-29 16:54:49 +0900

    2022-10-29 JpaAuditing문제 JpaConfig 생성

--- src/main/java/com/dodo/bootawsproject/BootAwsProjectApplication.java
+++ src/main/java/com/dodo/bootawsproject/BootAwsProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // jpa auditing 활성화
+// @EnableJpaAuditing // jpa auditing 활성화 > 테스트문제 때문에 삭제.
 @SpringBootApplication
 public class BootAwsProjectApplication {
 
